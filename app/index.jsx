ffda7e9e6e8d08eb04af1a8c508f43cae1cf0d3e
AkhatorEnosa <osa@John-316.local>
2025-01-12 00:12:05 +0100

    LocalStorage with AsyncStorage

--- app/index.jsx
+++ app/index.jsx
@@ -3,18 +3,84 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { data } from '@/data/todos'
 import { Inter_500Medium, useFonts } from '@expo-google-fonts/inter'
 import Animated, { LinearTransition } from "react-native-reanimated";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar } from "react-native";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Octicons from '@expo/vector-icons/Octicons';
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ThemeContext } from "@/context/ThemeContext";
 
 export default function Index() {
   const { colorScheme, setColorScheme, theme } = useContext(ThemeContext)
 
-  const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id))
+  const [todos, setTodos] = useState([])
 
   const [inputText, setInputText] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+          const jsonValue = await AsyncStorage.getItem('TodoApp');
+
+          const storageTodos = jsonValue !== null ? JSON.parse(jsonValue) : null;
+
+          if(storageTodos) {
+            setTodos(storageTodos.sort((a, b) => b.id - a.id))
+          } else {
+            setTodos(data.sort((a, b) => b.id - a.id))
+          }
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+
+    const fetchTheme = async () => {
+      try {
+          const theme = await AsyncStorage.getItem('Theme');
+
+          const storageTheme = theme !== null ? theme : null;
+
+          if(storageTheme) {
+            setColorScheme(storageTheme)
+          } else {
+            setColorScheme(colorScheme)
+          }
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchData()
+    fetchTheme()
+  }, [data])
+
+  useEffect(() => {
+    const storeData = async () => {
+      try {
+          const jsonValue = JSON.stringify(todos)
+          await AsyncStorage.setItem("TodoApp", jsonValue)
+      } catch (error) {
+        console.log(error) 
+      }
+    }
+
+    const saveTheme = async () => {
+      try {
+        await AsyncStorage.setItem("Theme", colorScheme)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    storeData()
+    saveTheme()
+  }, [todos, colorScheme])
+  
+  
 
   const [loaded, error] = useFonts({
     Inter_500Medium,
@@ -33,7 +99,7 @@ export default function Index() {
   const footerComp = <Text style={styles.footer}>.</Text>
   
   const emptyList = <View style={styles.emptyList}>
-                      <MaterialCommunityIcons name="clipboard-list-outline" size={100} color="black" />
+                      <MaterialCommunityIcons name="clipboard-list-outline" size={100} color={theme.text} />
                       <Text style={styles.emptyListText}>No Todos.</Text>
                     </View>
   
@@ -65,6 +131,9 @@ export default function Index() {
     <Container>
       <View style={styles.overallContainer}>
         <View style={styles.inputContainer}>
+          <Pressable onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')} style={styles.toggleTheme}>
+            <Octicons name={colorScheme === 'dark' ? 'moon' : 'sun'} size={24} color={theme.text} selectable={undefined}/>
+          </Pressable>
           <TextInput
             style={styles.input}
             onChangeText={setInputText}
@@ -75,15 +144,13 @@ export default function Index() {
           <Pressable style={styles.inputButton} onPress={handleSubmit}>
             <Text style={styles.inputButtonText}>Add</Text>
           </Pressable>
-          <Pressable onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')} style={{ marginLeft: 10}}>
-            <Octicons name={colorScheme === 'dark' ? 'moon' : 'sun'} size={36} color={theme.text} selectable={undefined} style={{ width: 36 }}/>
-          </Pressable>
         </View>
         <Animated.FlatList  
           data={todos}
           keyExtractor={(todo) => todo.id}
           showsHorizontalScrollIndicator={false}
           showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.contentContainer}
           ItemSeparatorComponent={seperatorComp}
           ListFooterComponent={footerComp}
@@ -100,6 +167,7 @@ export default function Index() {
           keyboardDismissMode="on-drag"
         />
       </View>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
     </Container>
   );
 }
@@ -107,8 +175,10 @@ export default function Index() {
 function createStyles(theme, colorScheme) {
   return StyleSheet.create({
     overallContainer: {
-      paddingTop: 10,
+      paddingTop: 50,
       paddingBottom: 10,
+      paddingRight: 10,
+      paddingLeft: 10,
       width: '100%',
       height: '100%',
       backgroundColor: theme.background,
@@ -117,29 +187,37 @@ function createStyles(theme, colorScheme) {
     inputContainer: {
       width: '100%',
       maxWidth: 1024,
-      paddingLeft: 20,
-      paddingRight: 20,
       paddingTop: 10,
       backgroundColor: theme.background,
       flexDirection: 'row',
+      alignItems: "center",
+      justifyContent: "space-between",
       gap: 10,
       marginBottom: 20,
       pointerEvents: "auto" 
     },
     input: {
-      width: '85%',
+      flexGrow: 1,
+      minWidth: 0,
       borderWidth: 1,
       borderColor: colorScheme === 'dark' ? "grey" : "black",
       color: theme.text,
       padding: 5,
-      borderRadius: 5,
+      borderRadius: 10,
       height: 50,
       fontSize: 20,
       fontFamily: 'Inter_500Medium'
     },
+    toggleTheme: {
+      padding: 10,
+      borderRadius: 10,
+      backgroundColor: 'rgba(0,0,0,0.1)',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
     inputButton: {
-      width: '15%',
-      borderRadius: 5,
+      padding: 10,
+      borderRadius: 10,
       backgroundColor: theme.button,
       justifyContent: 'center',
       alignItems: 'center',
@@ -191,13 +269,15 @@ function createStyles(theme, colorScheme) {
       color: 'grey'
     },
     emptyList: {
+      height: '100vh',
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       fontSize: 20
     },
     emptyListText: {
-      fontSize: 20
+      fontSize: 20,
+      color: theme.text
     }
   })
 }
